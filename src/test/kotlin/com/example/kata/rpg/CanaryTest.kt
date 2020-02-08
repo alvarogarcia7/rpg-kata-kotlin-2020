0b599e6384d33f9293eebdbe69491f269f591f94
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-02-08 23:22:06 +0000

    Start kata

---
+++ src/test/kotlin/com/example/kata/rpg/CanaryTest.kt
@@ -0,0 +1,11 @@
+package com.example.kata.rpg
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class CanaryTest {
+    @Test
+    fun `it runs`(){
+        Assertions.assertThat(true).isTrue()
+    }
+}
