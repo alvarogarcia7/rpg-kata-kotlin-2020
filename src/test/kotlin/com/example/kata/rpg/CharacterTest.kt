a93b3da1b90475b262cfac1695e51cd456b60b21
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-02-09 02:11:37 +0000

    Finish iteration 2

--- src/test/kotlin/com/example/kata/rpg/CharacterTest.kt
+++ src/test/kotlin/com/example/kata/rpg/CharacterTest.kt
@@ -1,6 +1,7 @@
 package com.example.kata.rpg
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Ignore
 import org.junit.Test
 
 class CharacterTest {
@@ -65,15 +66,98 @@ class CharacterTest {
 
         assertThat(deadCharacter.alive).isFalse()
     }
+
+    @Test
+    fun `character can attack an enemy`() {
+        val character = Character()
+        val enemyHealth = 1000
+        val enemy = Character(enemyHealth)
+
+        character.attack(enemy, 500)
+
+        assertThat(enemy.health).isEqualTo(enemyHealth - 500)
+    }
+
+    @Test
+    fun `character can't attack himself`() {
+        val initialHealth = 1000
+        val character = Character(initialHealth)
+
+        character.attack(character, 500)
+
+        assertThat(character.health).isEqualTo(initialHealth)
+    }
+
+    @Test
+    fun `character will deal less damage if the enemy is five levels above`() {
+        val character = Character()
+        val enemyHealth = 1000
+        val enemyLevel = 6
+        val enemy = Character(enemyHealth, enemyLevel)
+
+        character.attack(enemy, 500)
+
+        assertThat(enemy.health).isEqualTo(enemyHealth - (500 / 2))
+    }
+
 }
 
-class Character(var health: Int = INITIAL_HEALTH) {
+class DamageEffectTest {
+    @Test
+    fun `damage effect will be reduced`() {
+        val damageEffect = damageEffectOf(1, 6, 2)
+
+        assertThat(damageEffect.amount).isEqualTo(1)
+    }
+
+    @Test
+    fun `damage effect will stay the same`() {
+        val damageEffect = damageEffectOf(1, 1, 2)
+
+        assertThat(damageEffect.amount).isEqualTo(2)
+    }
+
+    @Test
+    fun `damage effect will stay the same as long as the level difference is not 5`() {
+        val damageEffect = damageEffectOf(1, 5, 2)
+
+        assertThat(damageEffect.amount).isEqualTo(2)
+    }
+
+    @Test
+    fun `damage effect will be boosted when the difference in levels (case 1)`() {
+        val damageEffect = damageEffectOf(6, 1, 2)
+
+        assertThat(damageEffect.amount).isEqualTo((2 * 1.5).toInt())
+    }
+
+    @Test
+    fun `damage effect will be boosted when the difference in levels (case 2)`() {
+        val damageEffect = damageEffectOf(7, 1, 2)
+
+        assertThat(damageEffect.amount).isEqualTo((2 * 1.5).toInt())
+    }
+
+    @Test
+    fun `damage effect will stay the same (case 2)`() {
+        val damageEffect = damageEffectOf(4, 1, 2)
+
+        assertThat(damageEffect.amount).isEqualTo(2)
+    }
+}
+
+class DamageEffect(val amount: Int) {
+
+}
+
+class Character(health: Int = INITIAL_HEALTH, val level: Int = 1) {
     companion object {
         private val INITIAL_HEALTH = 1000
         private val MAXIMUM_HEALTH = 1000
     }
 
-    val level: Int = 1
+    var health = health
+        private set
     val alive: Boolean
         get() = health > 0
 
@@ -86,4 +170,21 @@ class Character(var health: Int = INITIAL_HEALTH) {
         if (!alive) return
         health = Math.min(MAXIMUM_HEALTH, health + amount)
     }
+
+    fun attack(enemy: Character, damage: Int) {
+        if (enemy == this) return
+
+        enemy.receiveDamage(damageEffectOf(level, enemy.level, damage).amount)
+    }
+}
+
+fun damageEffectOf(characterLevel: Int, enemyLevel: Int, damage: Int): DamageEffect {
+    if (characterLevel - enemyLevel >= 5) {
+        return DamageEffect((damage * 1.5).toInt())
+    }
+    if (enemyLevel - characterLevel >= 5) {
+        return DamageEffect(damage / 2)
+    }
+
+    return DamageEffect(damage)
 }
